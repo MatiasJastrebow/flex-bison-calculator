0df6aa5e0d2b031930c9cec2182e14ecab523e68
Julián Cardoso Landin <jcardosolandin@frba.utn.edu.ar>
2023-09-22 16:26:47 -0300

    Update main.c
    Nuevo main

--- TP1/main.c
+++ TP1/main.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 #include "scanner.h"
 
-int main(){ 
+int main() {
     int i;
     enum token salida;
-    do{
-        salida = scanner(&i);  // cada vez que termina la ejecución se utiliza el valor "pasado por referencia" del índice
+    salida = scanner(&i);
 
-        mostrar_lexema(lexema , i);
+    while( salida!= EOFILE) {
 
-        if(salida == IDENTIFICADOR){
-            printf("ES UN IDENTIFICADOR\n");
-        }
-        else if(salida == ENTERO){
-            printf("ES UN ENTERO\n");
-        }
-        else if(salida == HEXADECIMAL){
-            printf("ES UN HEXADECIMAL\n");
+        mostrar_lexema(lexema, i);
+
+        switch(salida){
+            case IDENTIFICADOR:
+                printf("ES UN IDENTIFICADOR\n");
+
+            case ENTERO:
+                printf("ES UN ENTERO\n");
+
+            case HEXADECIMAL:
+                printf("ES UN HEXADECIMAL\n");
+
+            case ERROR_GEN:
+                printf("ERROR_GEN\n");
+
+            case ERROR_ENTERO:
+                printf("ERROR_ENTERO\n");
         }
 
-        else if(salida == ERROR_GEN){
-            printf("ERROR_GEN\n");
-        }
-        else if(salida == ERROR_ENTERO){
-            printf("ERROR_ENTERO\n");
-        }
-        
-        else{
-            printf("OCURRIO UN ERROR INESPERADO\n");
-        }
-        
+        salida = scanner(&i);
+    }
 
-    }while(salida != EOFILE);
     return 0;
 }
